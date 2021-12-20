c6da0d55f9860f65813e403976538587a2415bd2
Marcus Blackhall <marcusinamsterdam@gmail.com>
2021-12-20 11:16:12 +0100

    Inititial commit
    Project with webflux and lombok

---
+++ src/main/java/com/iamatum/iamatumreactivestreams/ReactivestreamsApplication.java
@@ -0,0 +1,13 @@
+package com.iamatum.iamatumreactivestreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivestreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivestreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamatum/iamatumreactivestreams/ReactivestreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamatum.iamatumreactivestreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivestreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
